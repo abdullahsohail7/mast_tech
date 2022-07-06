06890e7e096eee20410217a650256006facba0ed
Syed Abdullah Abbas Sohail <abdullahsohail7@hotmail.com>
2022-07-06 14:25:58 +0500

    mast-tech

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='border-t border-white bg-gray-900'>
-        <div className='my-1 max-w-screen-xl mx-auto bg-gray-900'>
+        <div className='my-1 max-w-screen-full mx-auto bg-gray-900'>
             <div className='grid grid-cols-2 lg:grid-cols-3 text-center gap-12 bg-gray-900'>
                 <div>
                 <h1 className='text-lg font-sans font-semi-bold text-blue-900'>
@@ -21,12 +21,12 @@ const Footer = () => {
                 
                 </div>
                 <div>
-                <h1 className='text-lg font-sans font-semi-bold text-blue-900'>
+                <h1 className='text-lg font-sans font-semi-bold text-blue-900 text-center'>
                     UK Representative
                 </h1>
                 <ul className='mt-1 font-sans font-xs text-gray-600'>
                     
-                    <li>uk@masttechnologies.com</li>
+                    <li className='text-center'>uk@masttechnologies.com</li>
                     <li>+447541055014</li>
                   
             
@@ -36,12 +36,12 @@ const Footer = () => {
                 
                 </div>
                 <div>
-                <h1 className='text-lg font-sans font-semi-bold text-blue-900'>
+                <h1 className='text-lg font-sans font-semi-bold text-blue-900 text-center'>
                     Company Contact
                 </h1>
                 <ul className='mt-1 font-sans font-xs text-gray-600'>
                     
-                    <li>hello@masttechnologies.com</li>
+                    <li className='text-center'>hello@masttechnologies.com</li>
                     <li>+923005603366</li>
                   
             
