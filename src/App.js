6f1ed24b1e6f98bfd57c7cf961b9ec3a0f7beff2
Abdullah <syedabdullahsohail7@gmail.com>
2024-08-31 23:02:57 +0500

    xyz pro7

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
@@ -6,21 +7,37 @@ import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import Team from "./components/Team";
 import WhyMast from "./components/WhyMast";
-
+import GetXYZ from "./components/GetXYZ"; // Import the new component
 
 function App() {
+  const [isMenuShown, setIsMenuShown] = useState(false);
 
-  const [isMenuShown, setIsMenuShown]= useState(false);
   return (
-    <div>
-      <Navbar isMenuShown={isMenuShown} setIsMenuShown={setIsMenuShown}/>
-      <HeroSection isMenuShown={isMenuShown}/>
-      <About/>
-      <Team/>
-      <WhyMast/>
-      <Contact/>
-      <Footer/>
-    </div>
+    <Router>
+      <Navbar isMenuShown={isMenuShown} setIsMenuShown={setIsMenuShown} />
+      <Routes>
+        {/* Home Page Route */}
+        <Route 
+          path="/" 
+          element={
+            <>
+              <HeroSection isMenuShown={isMenuShown} />
+              <About />
+              <Team />
+              <WhyMast />
+              <Contact />
+              <Footer />
+            </>
+          } 
+        />
+        
+        {/* /get Route */}
+        <Route path="/get" element={<GetXYZ />} />
+
+        {/* You can define other routes as needed */}
+        {/* Example: <Route path="/about" element={<About />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
