26e428409d65930391690396e4ec22e333eb7f23
Abdullah <syedabdullahsohail7@gmail.com>
2024-08-31 18:55:50 +0500

    xyz pro4

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ import codeImage from '../assets/code.jpg'
 const About = () => {
   return (
     
-   <PageSection name='about' id='about' title='About' subtitle={`XYZ Technologies is the most renowned remote solution provider in Pakistan. It has its offices located in London, Islamabad and New York`} >
+   <PageSection name='about' id='about' title={<h2 className='text-4xl border-b-4 border-gray-500 inline-block pb-2'>About</h2>} subtitle={`XYZ Technologies is the most renowned remote solution provider in Pakistan. It has its offices located in London, Islamabad and New York`} >
 <div className='flex flex-col lg:flex-row items-center justify-center gap-8 font-sans text-white'>
   <div className='flex flex-col gap-4 w-full lg:w-1/2 p-8 bg-gray-900 rounded-lg text-white'>
     <p className='border-l-4 pl-4 odd:border-thOrange even:border-red-800 text-white'>
--- src/components/WhyMast.jsx
+++ src/components/WhyMast.jsx
@@ -14,58 +14,54 @@ const WhyXYZ = () => {
             src: prize,
             title: "World-class experience",
             subtitle: 'We are obsessed with creating a delightful user experience for you and your team every step of the way—and it shows.',
-            style: "shadow-red-800",
+            style: "shadow-red-900",
           },
           {
             id: 2,
             src: usd,
             title: "Fair Price Guarantee",
             subtitle:"No hidden fees, deposit requirements, surprise costs, minimums, contract lengths, or exclusivity agreements. Ever.",
-            style: "shadow-yellow-800",
+            style: "shadow-yellow-900",
           },
           {
             id: 3,
             src: usery,
             title: "People with Passion",
             subtitle:"Our compliance experts are on the ground in every continent, proactively monitoring and improving processes for you.",
-            style: "shadow-green-800",
+            style: "shadow-green-900",
           },
           {
             id: 4,
             src: shield,
             title: "Protecting IP at all costs",
             subtitle:"Trust our stronger-than-they-need-to-be intellectual property and invention rights protections.",
-            style: "shadow-blue-800",
+            style: "shadow-blue-900",
           },
           {
             id: 5,
             src: sidebar,
             title: "Availability around the clock ",
             subtitle:"End users expect always on and incredible speed. We match with continuous ways to track consumer feedback, fix bugs, and release the latest and greatest product.",
-            style: "shadow-orange-800",
+            style: "shadow-orange-900",
           },
           {
             id: 6,
             src: buidling,
             title: "Prepared for Future",
             subtitle:"Automattic, Buffer, GitLab, Invision, and Zapier all have from 100 to nearly 1,000 remote employees.",
-            style: "shadow-white",
+            style: "shadow-purple-900",
           },
         
         ];
     
   return (
-   <PageSection name='Why XYZ' title='Why XYZ' subtitle='Why you should choose XYZ Technologies'>
+   <PageSection name='Why XYZ' title={<h2 className='text-4xl border-b-4 border-gray-500 inline-block pb-2'>Why XYZ</h2>} subtitle='Why you should choose XYZ Technologies'>
          
-
-
-   
-
-        <div className="bg-gradient-to-r from-blue-900 to-thBlue w-screen-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0 text-white">
+        <div className="bg-gray-900 w-[calc(100%+4rem)] h-screen -mx-8 px-8 sm:px-12 py-20 grid grid-cols-2 sm:grid-cols-3 gap-8 text-center text-white rounded-lg">
           {WhyM.map(({ id, src, title,subtitle, style }) => (
             <div
               key={id}
-              className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
+              className={`shadow-md hover:scale-105 duration-500 py-4 px-4 rounded-lg ${style}`}
             >
               <img src={src} alt="" className="w-20 mx-auto" />
               <p className="mt-4 text-lg">{title}</p>
@@ -73,21 +69,7 @@ const WhyXYZ = () => {
             </div>
           ))}
         </div>
-      
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        
    </PageSection>
   )
 }
